ff1875e210198622fc2734f06c0d8464e66fb5ab
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-01-02 00:05:24 +0530

    medium commit

--- week-2/01-async-js/medium/file-cleaner.js
+++ week-2/01-async-js/medium/file-cleaner.js
@@ -1,7 +1,10 @@
-"use strict";
+import fs from "fs";
 
-const fs = require("fs");
-
-fs.readFile('test.txt', 'utf-8', (data, err) => {
-    
+fs.readFile('test.txt', 'utf-8', (err, data) => {
+    try{
+        const newData = data.split(' ').filter((word) => (word !== '')).join(' ');
+        console.log(newData);
+    } catch(err){
+        throw err;
+    }
 });
